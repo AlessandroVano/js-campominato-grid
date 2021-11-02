b61933816f107ead499a59a1d52c6fae50ca3421
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-02 17:31:13 +0100

    inserimento costanti btn & griglia

--- js/main.js
+++ js/main.js
@@ -6,5 +6,14 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
  */
+// inserimento const del bottone 'gioca'
+const btn = document.getElementById('setdimension');
+console.log(setdimension);
+// inserimento class 'griglia' perchè devo togliere il testo quando andrò a cliccare il bottone
+const griglia = document.querySelector('.griglia');
+console.log(griglia);
+
+
 
