b1c3f210bd3fd928d5f91dfcb3f5fb1cf6c9d889
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-02 18:22:25 +0100

    inserimento constante livello difficoltà, inserimento costante per definire difficoltà scelta

--- js/main.js
+++ js/main.js
@@ -8,31 +8,39 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 
-  lista cose da fare in sequenza
+  lista cose da fare in sequenza:
+
   1. inserimento costante bottone
   2. impostazione evento click del bottone
   3. inserimento costante per eliminare l'h2 e successivamente ci servirà per generare la griglia
   4. inserimento regolina per pulire 
-  5. creazione costante per impostare le griglie
+  5. creazione costante per impostare il livello di difficoltà
+  6. inserimento costante (all'interno del evento click, quindi andrà a cascata) per definirgli che opzione di difficoltà hai scelto
 
 
 
 
  */
-// inserimento const del bottone 'gioca'
+// 1. inserimento const del bottone 'gioca'
 const setBtn = document.getElementById('imposta-difficolta');
 console.log(setBtn);
-// inserimento costante per eliminare l'h2 e successivamente ci servirà per generare la griglia
+// 3. inserimento costante per eliminare l'h2 e successivamente ci servirà per generare la griglia
 const griglia = document.querySelector('.griglia');
-//creazione costante per impostare le griglie
+console.log(griglia);
+// 5. creazione costante per impostare il livello di difficoltà
+const difficoltaLivello = document.getElementById('difficolta');
+console.log(difficoltaLivello);
 
 
 // SETTAGGIO GRIGLIA
-//impostazione evento bottone click
+// 2. impostazione evento bottone click
 setBtn.addEventListener('click', () => {
     console.log('click');
-    //eliminazione del contenuto dentro 'griglia'
+    // 4. eliminazione del contenuto dentro 'griglia'
     griglia.innerHTML = '';
+    // 6. inserimento costante (all'interno del evento click, quindi andrà a cascata) per definirgli che opzione di difficoltà hai scelto
+    const sceltaDifficoltà = difficoltaLivello.value;
+    console.log(sceltaDifficoltà);
 });
 
 
