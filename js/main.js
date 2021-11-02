6cb8d4c3b4e6162a9b3edadbd5d943794d5e6951
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-02 17:52:28 +0100

    inserimento const per eliminare l'h2 + eliminazione contenuti all'interno della griglia al click

--- js/main.js
+++ js/main.js
@@ -7,16 +7,28 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+
+  lista cose da fare in sequenza
+  1. inserimento costante bottone
+  2. impostazione evento click del bottone
+  3. inserimento costante per eliminare l'h2 e successivamente ci servirà per generare la griglia
+
+
+
+
  */
 // inserimento const del bottone 'gioca'
 const btn = document.getElementById('setdimension');
 console.log(setdimension);
+// inserimento costante per eliminare l'h2 e successivamente ci servirà per generare la griglia
+const griglia = document.querySelector('.griglia')
 
-// settaggio griglia
+// SETTAGGIO GRIGLIA
 //impostazione evento bottone click
 btn.addEventListener('click', () => {
     console.log('click');
-
+    //eliminazione del contenuto dentro 'griglia'
+    griglia.innerHTML = '';
 });
 
 
